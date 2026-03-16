e3b42b708b46153ba336c90846f545d395672cf8
point50 <pones0050@gmail.com>
2026-03-16 16:01:22 +0700

    fixing build issues

--- src/components/VenueCatalog.tsx
+++ src/components/VenueCatalog.tsx
@@ -1,13 +1,15 @@
 import Link from "next/link";
 import Card from "./Card";
 
-export default function VenueCatalog({venuesJson}:{venuesJson: Promise<VenueJson>}) {
+export default async function VenueCatalog({venuesJson}:{venuesJson: Promise<VenueJson>}) {
+    const data = await venuesJson;
+
     return (
         <>
-        Explore {venuesJson.count} venues in out catalog
+        Explore {data.count} venues in out catalog
         <div style = {{margin : "20px", display: "flex", flexDirection: "row", alignContent: "space-around", justifyContent: "space-around", flexWrap: "wrap", padding: "10px"}}>
             {
-                venuesJson.data.map((venueItem:Object) => (
+                data.data.map((venueItem:VenueItem) => (
                     <Link href = {`/venue/${venueItem.id}`} key = {venueItem.id} className="w-1/5">
                         <Card venueName={venueItem.name} imgSrc={venueItem.picture} />
                     </Link>
